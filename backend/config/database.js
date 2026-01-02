c7cb8fca7d102f4a12f708e137d7830b641e7ea8
Alan Roca <alan.roca1410@gmail.com>
2026-01-02 14:57:31 +0100

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,5 +4,11 @@ dotenv.config();
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   logging: false,
 });
